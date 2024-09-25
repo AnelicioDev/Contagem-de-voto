f94bf06d70db829e36eefaa30a3d34722f9b326f
AnelicioDev <anelicio.dev@gmail.com>
2024-09-25 20:59:21 -0300

    inicioprojeto

--- Contagem-de-votos/src/App.java
+++ Contagem-de-votos/src/App.java
@@ -1,5 +1,17 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        String caminhoArquivo = "C:\\Users\\aneli\\OneDrive\\Documentos\\Cursos\\Java\\Contagem-de-votos\\Votos.txt";
+
+        BufferedReader br = new BufferedReader(new FileReader(caminhoArquivo));
+
+        String linhas;
+        while ((linhas = br.readLine()) != null) {
+            System.out.println(linhas);
+        }
+
+        br.close();
     }
 }
